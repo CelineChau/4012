0d2444ad3f08e7a2ca2e8019da765bc1f9ea2b95
Axel Razafimahefa <axel.raza12@gmail.com>
2022-04-30 18:03:11 +0200

    implement sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import Section from '../components/section';
+import React, { useState } from 'react';
+import Products from '../components/List/Products';
+import SearchBar from '../components/SearchBar/SearchBar';
+import Section from '../components/Section/Section';
 
 const Home = () => {
+    const [product, setProduct] = useState("Tomato");
+    const [nbProducts, setNbProducts] = useState(0);
+
+    const [categories, setData] = useState([
+        {
+            category: 'Land use change',
+            value: 0
+        },
+        {
+            category: 'Animal feed',
+            value: 0
+        },
+        {
+            category: 'Farm',
+            value: 0
+        },
+        {
+            category: 'Processing',
+            value: 0
+        },
+        {
+            category: 'Transport',
+            value: 0
+        },
+        {
+            category: 'Packaging',
+            value: 0
+        },
+        {
+            category: 'Retail',
+            value: 0
+        }
+    ]);
+
     return (
-        <div>
-            <Section></Section>
-        </div>
+      <div className="container mx-auto">
+        <SearchBar setValue={setProduct} />
+        <Products size={setNbProducts} search={product}></Products>
+        {
+            nbProducts > 0 ? categories.map((oCategory, iIndex) => <Section index={iIndex} information={oCategory}></Section>) : null
+        }
+      </div>
     );
 };
 
