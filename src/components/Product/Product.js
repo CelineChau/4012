e15b8bf3cecc9171b4b00c1c7cf1b2818e96c16c
Celine Chau <celine.chau@withings.com>
2022-04-30 20:35:42 +0200

    [product] update product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const Product = ({info}) => {
+const Product = ({info, setName}) => {
+    const [productInfo, setProductInfo] = useState("");
+
     return (
-        <li class="p-3 hover:bg-blue-600 hover:text-blue-200">
+        <li 
+            className="p-3 hover:bg-blue-600 hover:text-blue-200"
+            onClick={() => setName(info.Food_product)}
+        >
             <div className="product">
                 <p>{info.Food_product}</p>
             </div>
